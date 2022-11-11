36cd888d599f99d9589f8573da966224978d5365
Ibrahim <ibrachar68@gmail.com>
2022-11-11 15:51:35 +0100

    tasto next

---
+++ main.js
@@ -0,0 +1,21 @@
+let arrayimg=["img/01.webp", "img/02.webp","img/03.webp","img/04.webp","img/05.webp"]
+let i = 0;
+ const next=document.querySelector(".next");
+ const prev=document.querySelector(".prev");
+ let slide = document.getElementById("slide");
+
+slide.src = arrayimg[i];
+next.addEventListener("click",
+ function(){
+    i++;
+  if (i > arrayimg.length - 1) {
+    i = 0;
+  }
+  slide.src = arrayimg[i];
+ }
+
+);
+
+
+
+
