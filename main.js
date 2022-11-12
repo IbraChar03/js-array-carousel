0a5b899663d404cfd8fa86d916b483f963799d90
Ibrahim <ibrachar68@gmail.com>
2022-11-12 11:54:04 +0100

    funzione tasti sopra e sotto

--- main.js
+++ main.js
@@ -1,31 +1,34 @@
 let arrayimg=["img/01.webp", "img/02.webp","img/03.webp","img/04.webp","img/05.webp"]
 let i = 0;
- const next=document.querySelector(".next");
  const prev=document.querySelector(".prev");
+ const next=document.querySelector(".next");
  let slide = document.getElementById("slide");
 
-slide.src = arrayimg[i];
-next.addEventListener("click",
- function(){
-    i++;
+ slide.src = arrayimg[i];
+
+ next.addEventListener("click",
+   function(){
+     i++;
    if (i > arrayimg.length - 1) {
     i = 0;
-   }
-   slide.src = arrayimg[i];
- }
-
-);
-
-prev.addEventListener("click",
- function(){
-    i--;
-  if (i < 0) {
-    i = arrayimg.length - 1;
+    }
+    slide.src = arrayimg[i];
   }
-  slide.src = arrayimg[i];
- }
+
+ );
+
+
+ prev.addEventListener("click",
+ function(){
+  i--;
+if (i < 0) {
+  i = arrayimg.length - 1;
+}
+slide.src = arrayimg[i];
+}
 
 );
+
 
 
 
